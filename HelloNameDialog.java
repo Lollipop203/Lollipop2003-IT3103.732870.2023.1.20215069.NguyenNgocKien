882e647c112e9886d08b5b8c9cc40e1dbbbbcd05
Lollipop203 <147888600+Lollipop203@users.noreply.github.com>
2023-10-16 22:30:43 +0700

    Add files via upload

---
+++ HelloNameDialog.java
@@ -0,0 +1,15 @@
+
+//ex3: HelloNameDialog.java
+import javax.swing.*;
+
+public class HelloNameDialog {
+	public static void main(String[] args) {
+		String result;
+		result = JOptionPane.showInputDialog("NguyenNgocKien-Hay nhap ten cua ban");
+		JOptionPane.showMessageDialog(null, "Toi la NguyenNgocKien. Hi "+result+"!");
+		System.exit(0);
+	}
+	
+
+}
+// Nguyen Ngoc Kien 20215069
