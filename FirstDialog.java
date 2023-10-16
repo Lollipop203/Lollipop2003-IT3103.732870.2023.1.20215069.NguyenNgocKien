417fdf41379839520bfd7649dc1a33657768ae96
Lollipop203 <147888600+Lollipop203@users.noreply.github.com>
2023-10-16 22:30:19 +0700

    Add files via upload

---
+++ FirstDialog.java
@@ -0,0 +1,9 @@
+//ex2:firstdialog.java
+import javax.swing.*;
+public class FirstDialog {
+	public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"NguyenNgocKien-20215069-Hello world!How are you");
+    System.exit(0);
+	}
+}
+//Nguyen Ngoc Kien 2021509
